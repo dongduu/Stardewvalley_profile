99074b27f07b8d1fb4edf61fc917af6fbb54ebb2
dongduu <4012popo@naver.com>
2022-08-24 16:46:03 +0900

    feat: NavBar style

--- stardewvalley__profile/components/NavBar.tsx
+++ stardewvalley__profile/components/NavBar.tsx
@@ -6,14 +6,15 @@ export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav>
+    <nav className={styles.box}>
       <Link href="/">
         <img
+          className={styles.logo}
           src="https://stardewvalley.net/wp-content/uploads/2017/12/med_logo.png"
           alt="logo"
         />
       </Link>
-      <div>
+      <div className={styles.item}>
         <Link href="/">
           <a
             className={`${styles.link} ${
@@ -44,6 +45,7 @@ export default function NavBar() {
         <Link href="https://www.stardewvalley.net/blog/">
           <a>
             <img
+              className={styles.homepage}
               src="https://secure.gravatar.com/avatar/f7ef77bb41d38cd162fceb30625d6289?s=75&d=http%3A%2F%2Fwww.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D75&r=G"
               alt="homepage"
             />
