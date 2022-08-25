583b47adede898ab2301210ef7fd1154b70b00d5
dongduu <4012popo@naver.com>
2022-08-25 13:18:30 +0900

    feat: Character card style

--- stardewvalley__profile/components/Character.tsx
+++ stardewvalley__profile/components/Character.tsx
@@ -1,17 +1,22 @@
+import styles from "../styles/Character.module.scss";
+
 export default function Character() {
   return (
-    <div>
+    <div className={styles.box}>
       <div>
-        <img src="https://w.namu.la/s/2f015d5f3bef253a62c2c1c7597afc0250e1afb58fb35706be401b14b28160cb8913a14d3f757ce4dc43158eb43254ad468559ff2d2bebe2d29e97632256b30836872a84af4aeb2ebff7cc2334ead4c432a6f3063147020b4bf98e7776e7ef6f" />
-        <h2>Harvey</h2>
+        <img
+          className={styles.profile_img}
+          src="https://w.namu.la/s/2f015d5f3bef253a62c2c1c7597afc0250e1afb58fb35706be401b14b28160cb8913a14d3f757ce4dc43158eb43254ad468559ff2d2bebe2d29e97632256b30836872a84af4aeb2ebff7cc2334ead4c432a6f3063147020b4bf98e7776e7ef6f"
+        />
+        <h2 className={styles.profile_name}>Harvey</h2>
       </div>
-      <ul>
-        <li>brith: 14 winter</li>
-        <li>job: doctor</li>
-        <li>family: X</li>
-        <li>friends: Maru</li>
+      <ul className={styles.ul}>
+        <li className={styles.li}>brith: 14 winter</li>
+        <li className={styles.li}>job: doctor</li>
+        <li className={styles.li}>family: X</li>
+        <li className={styles.li}>friends: Maru</li>
       </ul>
-      <div>+</div>
+      <div className={styles.plus}>+</div>
     </div>
   );
 }
