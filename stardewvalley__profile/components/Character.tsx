8cdcc5beea9afcf16f29a0d5fca6923332345d2e
dongduu <4012popo@naver.com>
2022-08-25 12:37:38 +0900

    feat: Character component

---
+++ stardewvalley__profile/components/Character.tsx
@@ -0,0 +1,17 @@
+export default function Character() {
+  return (
+    <div>
+      <div>
+        <img src="https://w.namu.la/s/2f015d5f3bef253a62c2c1c7597afc0250e1afb58fb35706be401b14b28160cb8913a14d3f757ce4dc43158eb43254ad468559ff2d2bebe2d29e97632256b30836872a84af4aeb2ebff7cc2334ead4c432a6f3063147020b4bf98e7776e7ef6f" />
+        <h2>Harvey</h2>
+      </div>
+      <ul>
+        <li>brith: 14 winter</li>
+        <li>job: doctor</li>
+        <li>family: X</li>
+        <li>friends: Maru</li>
+      </ul>
+      <div>+</div>
+    </div>
+  );
+}
--- stardewvalley__profile/pages/index.tsx
+++ stardewvalley__profile/pages/index.tsx
@@ -1,5 +1,11 @@
+import Character from "../components/Character";
 import Seo from "../components/Seo";
 
 export default function Home() {
-  return <Seo title="Home" />;
+  return (
+    <>
+      <Seo title="Home" />
+      <Character />
+    </>
+  );
 }
