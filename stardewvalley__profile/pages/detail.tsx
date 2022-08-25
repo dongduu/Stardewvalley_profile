943be8b39000710103476f49c7e2529436fbf4a4
dongduu <4012popo@naver.com>
2022-08-26 00:17:37 +0900

    feat: DetailCard markup

--- stardewvalley__profile/pages/detail.tsx
+++ stardewvalley__profile/pages/detail.tsx
@@ -1,10 +1,11 @@
+import DetailCard from "../components/DetailCard";
 import Seo from "../components/Seo";
 
 export default function Detail() {
   return (
     <>
       <Seo title="About" />
-      "detail"
+      <DetailCard />
     </>
   );
 }
