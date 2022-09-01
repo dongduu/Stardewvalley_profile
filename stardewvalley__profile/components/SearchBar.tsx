416ebb82ac785a8f1f54d80a4640c54bcdd6f666
dongduu <4012popo@naver.com>
2022-09-01 18:47:32 +0900

    feat: style SearchBar

--- stardewvalley__profile/components/SearchBar.tsx
+++ stardewvalley__profile/components/SearchBar.tsx
@@ -1,8 +1,12 @@
+import styles from "../styles/SearchBar.module.scss";
+
 export default function SearchBar() {
   return (
-    <div>
-      <input></input>
-      <button>Search</button>
+    <div className={styles.box}>
+      <form className={styles.form}>
+        <input placeholder="Search..." className={styles.input}></input>
+        <button className={styles.button}>Search</button>
+      </form>
     </div>
   );
 }
