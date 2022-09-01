3588e6f51cc9752c6a0cc6c3df764cc533656bf2
dongduu <4012popo@naver.com>
2022-09-01 18:18:55 +0900

    feat: add SearchBar markup

--- stardewvalley__profile/pages/detail.tsx
+++ stardewvalley__profile/pages/detail.tsx
@@ -1,10 +1,12 @@
 import DetailCard from "../components/DetailCard";
+import SearchBar from "../components/SearchBar";
 import Seo from "../components/Seo";
 
 export default function Detail() {
   return (
     <>
       <Seo title="About" />
+      <SearchBar />
       <DetailCard />
     </>
   );
