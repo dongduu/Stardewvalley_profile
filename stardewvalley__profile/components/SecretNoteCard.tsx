cc972dee8dce81ea39336bd6b2f2cd7f72cfc195
dongduu <4012popo@naver.com>
2022-09-01 16:30:02 +0900

    feat: add secret card arrow

--- stardewvalley__profile/components/SecretNoteCard.tsx
+++ stardewvalley__profile/components/SecretNoteCard.tsx
@@ -1,17 +1,22 @@
 import styles from "../styles/SecretNoteCard.module.scss";
+import { MdArrowLeft, MdArrowRight } from "react-icons/Md";
 
 export default function SecretNoteCard() {
   return (
-    <div className={styles.box}>
-      <h2 className={styles.title}>Secret Note #1</h2>
-      <div className={styles.content}>
-        <p className={styles.contents}>
-          애비게일의 일기장 한 페이지입니다. <br />
-          '내가 사랑하는 것들 : 깍아낸 호박 냄새, 베개 밑에 둔 자수정, 초콜릿
-          케이크,매콤한 장어의 스릴 그리고 엄마가 해주신 블랙베리 코블러가 주는
-          편안함 (먹는게 좋은 걸!)'
-        </p>
+    <>
+      <MdArrowLeft size={120} className={styles.arrow_left} />
+      <MdArrowRight size={120} className={styles.arrow_right} />
+      <div className={styles.box}>
+        <h2 className={styles.title}>Secret Note #1</h2>
+        <div className={styles.content}>
+          <p className={styles.contents}>
+            애비게일의 일기장 한 페이지입니다. <br />
+            '내가 사랑하는 것들 : 깍아낸 호박 냄새, 베개 밑에 둔 자수정, 초콜릿
+            케이크,매콤한 장어의 스릴 그리고 엄마가 해주신 블랙베리 코블러가
+            주는 편안함 (먹는게 좋은 걸!)'
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
